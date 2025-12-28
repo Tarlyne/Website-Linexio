8585bddbb9534e56c0cd6d77ce7dd8ecfefd07e3
Torben <thor0907@gmx.de>
2025-12-28 20:35:16 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+// trigger rebuild
 export default defineConfig({
   plugins: [react()],
   // Leerer String sorgt für komplett relative Pfade in der index.html (best practice für GH Pages)
